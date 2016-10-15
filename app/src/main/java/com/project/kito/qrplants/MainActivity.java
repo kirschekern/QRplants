af45c266197e863008ac3d0796c40fa8c6144716
kirsten <kirn1@gmx.de>
2016-10-15 22:02:20 +0200

    Update1

---
+++ app/src/main/java/com/project/kito/qrplants/MainActivity.java
@@ -0,0 +1,16 @@
+package com.project.kito.qrplants;
+
+import android.content.SyncStatusObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Test1234
+        System.out.println("Hallo Welt");
+    }
+}
